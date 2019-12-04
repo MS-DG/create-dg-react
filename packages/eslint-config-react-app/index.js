f6d0c6e4e36274922d5c6bcb76656fbc47fbbeac
New Future (Weilai LIU) <New.Future@microsoft.com>
2019-12-04 20:39:39 +0800

    remove flowtype

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -312,8 +312,8 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
 
     // https://github.com/gajus/eslint-plugin-flowtype
-    'flowtype/define-flow-type': 'warn',
-    'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn',
+    // 'flowtype/define-flow-type': 'warn',
+    // 'flowtype/require-valid-file-annotation': 'warn',
+    // 'flowtype/use-flow-type': 'warn',
   },
 };
