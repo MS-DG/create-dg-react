e66a335f67ac998b8a3c4a0473e9816deb3b52fe
NewFuture <New.Future@microsoft.com>
2019-12-10 00:22:28 +0800

    format staged

--- packages/react-scripts/scripts/format.js
+++ packages/react-scripts/scripts/format.js
@@ -5,9 +5,13 @@
 // format --staged
 // format --check
 
-const fs = require('fs');
-// const path = require('path');
+// const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
 const prettier = require('prettier');
+const eslint = require('eslint');
+
+// const util = require('util');
 const listStaged = require('./utils/listStaged');
 
 let argv = process.argv.slice(2);
@@ -17,20 +21,58 @@ const isStaged =
     argv.find(a => a === 'staged' || a === '--staged'));
 
 // use the same config for all files
-const prettierOptions = prettier.resolveConfig.sync();
+
+// const prettierOptions = prettier.resolveConfig.sync(process.cwd());
 // todo concurrent;
+// warn as error;
+
+const eslinter = new eslint.Linter();
+function getStagedContent(f) {
+  // return util.promisify(cp.exec)(`git show :"${f}"`).then(f => f.stdout);
+  const buffer = cp.execSync(`git show :"${f}"`);
+  return buffer.toString();
+}
+
+function prettierCheck(f, content) {
+  const options = prettier.resolveConfig.sync(f);
+  options.filepath = f;
+  if (!prettier.check(content, options)) {
+    console.error('prettier:[×] ', f);
+    return false;
+  }
+  return true;
+}
+
+function lintCheck(file, content) {
+  const ext = path.extname(file);
+  if (['js', 'ts', 'jsx', 'tsx'].includes(ext)) {
+    const errs = eslinter.verify(content, {
+      filename: file,
+    });
+    if (errs && errs.length > 0) {
+      console.error('eslint:[×] ', file);
+      errs.forEach(msg =>
+        console.error(`  ${msg.line},${msg.column}:`, msg.message, msg.ruleId)
+      );
+      return false;
+    }
+  }
+  return true;
+}
 
 let isFail = false;
 if (isStaged) {
   listStaged().forEach(f => {
-    const input = fs.readFileSync(f, 'utf8');
-    if (!prettier.check(input, prettierOptions)) {
-      console.error('prettier:[×] ', f);
+    const content = getStagedContent(f);
+    if (prettierCheck(f, content) && lintCheck(f, content)) {
+      console.log('√', f);
+    } else {
       isFail = true;
     }
   });
 }
 
 if (isFail) {
+  console.error('lint staged fail, `npm run format` to autofix !');
   process.exit(1);
 }
