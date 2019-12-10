57584945c6472b8e0ea518752d9e856b2d6ffb83
New Future (Weilai LIU) <New.Future@microsoft.com>
2019-12-10 16:27:36 +0800

    rm yarn.lock.cached

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -313,23 +313,23 @@ function createApp(
     }
   }
 
-  if (useYarn) {
-    let yarnUsesDefaultRegistry = true;
-    try {
-      yarnUsesDefaultRegistry =
-        execSync('yarnpkg config get registry')
-          .toString()
-          .trim() === 'https://registry.yarnpkg.com';
-    } catch (e) {
-      // ignore
-    }
-    if (yarnUsesDefaultRegistry) {
-      fs.copySync(
-        require.resolve('./yarn.lock.cached'),
-        path.join(root, 'yarn.lock')
-      );
-    }
-  }
+  // if (useYarn) {
+  //   let yarnUsesDefaultRegistry = true;
+  //   try {
+  //     yarnUsesDefaultRegistry =
+  //       execSync('yarnpkg config get registry')
+  //         .toString()
+  //         .trim() === 'https://registry.yarnpkg.com';
+  //   } catch (e) {
+  //     // ignore
+  //   }
+  //   if (yarnUsesDefaultRegistry) {
+  //     fs.copySync(
+  //       require.resolve('./yarn.lock.cached'),
+  //       path.join(root, 'yarn.lock')
+  //     );
+  //   }
+  // }
 
   run(
     root,
