824d12238fe378c98aadd2e8f1475d773c384dc6
New Future (Weilai LIU) <New.Future@microsoft.com>
2019-12-18 15:15:37 +0800

    auth

--- packages/azure-devops-auth/index.js
+++ packages/azure-devops-auth/index.js
@@ -3,15 +3,18 @@
 'use strict';
 
 if (process.env.TF_BUILD || process.env.CI) {
-  console.log('skip: in CI enviroment');
+    console.log('skip: in CI enviroment');
 } else if (!require('fs').existsSync('.npmrc')) {
-  console.log('skip: .npmrc was not found');
+    console.log('skip: .npmrc was not found');
 } else if (process.platform === 'win32') {
-  // windows
-  require('child_process').execSync('npx vsts-npm-auth -c .npmrc', {
-    stdio: 'inherit',
-  });
+    // windows
+    require('child_process').execSync('npx vsts-npm-auth -c .npmrc', {
+        stdio: 'inherit',
+    });
 } else {
-  // mac or linux
-  require('./auth')();
+    // mac or linux
+    require('./auth')().catch(e => {
+        console.error(e);
+        process.exit(1);
+    });
 }
