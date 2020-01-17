637612a70efeaa7c7ebbd2f60f1d3a709cd4c952
New Future (Weilai LIU) <New.Future@microsoft.com>
2020-01-17 17:22:58 +0800

    index

--- packages/cra-template-dg-react/template/src/index.tsx
+++ packages/cra-template-dg-react/template/src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
+import  { hydrate, render }  from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./worker/serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.scss";
+
+const rootElement = document.getElementById("root")!;
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
