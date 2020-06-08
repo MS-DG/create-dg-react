ccaebec273d968bcad2bea532112696dbf676c2e
NewFuture <New.Future@microsoft.com>
2020-06-08 18:05:53 +0800

    set up

--- packages/cra-template-dg-react/template/src/setupTests.ts
+++ packages/cra-template-dg-react/template/src/setupTests.ts
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+if (document) {
+    document.createRange = () =>
+      (({
+        setStart: () => {},
+        setEnd: () => {},
+        commonAncestorContainer: {
+          nodeName: "BODY",
+          ownerDocument: document,
+        },
+      } as any) as Range);
+  }
+  
+  window.HTMLMediaElement.prototype.load = () => {};
+  window.HTMLMediaElement.prototype.play = () => new Promise<void>(resolve => resolve());
+  window.HTMLMediaElement.prototype.pause = () => new Promise<void>(resolve => resolve());
+  
