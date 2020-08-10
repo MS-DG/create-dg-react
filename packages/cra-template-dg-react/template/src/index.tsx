8d6a355123ff938329a6d96c57c165dece0295b4
NewFuture <New.Future@microsoft.com>
2020-08-10 18:30:20 +0800

    fix  prerender bugs

--- packages/cra-template-dg-react/template/src/index.tsx
+++ packages/cra-template-dg-react/template/src/index.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./worker/serviceWorker";
 
 import "./index.scss";
 
 const rootElement = document.getElementById("root")!;
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
-
+// TODO:
+// using hydrate for prerender
+// current has bugs when using hydrate API
+// it attached the elements not remove the sipnner
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
+render(<App />, rootElement, () => {
+  // remove prerender style
+  document.head.querySelectorAll("style[data-prerender]").forEach(s => s.remove());
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
