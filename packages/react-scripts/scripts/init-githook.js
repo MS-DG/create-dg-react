688c6e6ef0e0fbabb79c9091906c4537f69638db
New Future (Weilai LIU) <New.Future@microsoft.com>
2020-09-02 13:28:50 +0800

    inistall githook

---
+++ packages/react-scripts/scripts/init-githook.js
@@ -0,0 +1,35 @@
+// @remove-file-on-eject
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+    throw err;
+});
+const chalk = require('@dragongate/react-dev-utils/chalk');
+const execSync = require('child_process').execSync;
+const createGitHooks = require('./utils/createGitHooks');
+
+function isInGitRepository() {
+    try {
+        execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+
+module.exports = function () {
+    if (isInGitRepository()) {
+        createGitHooks();
+        console.log(chalk.green('git hooks are installed'));
+    }
+};
